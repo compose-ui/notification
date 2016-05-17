f6560a297bd0d5402d7d8b68137422a85dd5710c
Brandon Mathis <brandon@imathis.com>
2016-05-17 14:11:33 -0500

    Massive rewrite, removing old dependencies

--- test/index.test.js
+++ test/index.test.js
@@ -1,119 +1,77 @@
-var notify = require('../')
 var assert = require('chai').assert
-var animevent = require('compose-animevent')
-var sinon = require('sinon')
+
+var notify = require('../')
+var domify = require('domify')
+var event = require('compose-event')
+var $ = function(str) { return document.querySelector(str) }
+
+function clear(done){
+  this.msg.remove()
+  setTimeout(done, 10)
+}
 
 function assertMessage(type, message){
-  var el = document.querySelector('.notification.'+type+' .notification-message')
+  var el = $('.notification.'+type+' .notification-message')
   assert.isNotNull(el, 'Could not find '+'.notification.'+type+' .notification-message')
   assert.equal(el.innerHTML, message)
 }
 
-describe('notify', function() {
-  before(function(){
-    sinon.stub(animevent, 'one', function(el, eventType, callback){
-      // setTimeout(callback, 1)
-      callback()
+function testType(type) {
+  describe(type, function() {
+    it('shows a '+type+' message', function(){
+      var content = 'test '+type+' message'
+      if (type == 'default') {
+        this.msg = notify(content)
+        assertMessage('normal', content)
+      } else {
+        this.msg = notify[type](content)
+        assertMessage(type, content)
+      }
     })
+    after(clear)
   })
-  describe('default', function(){
-    before(function(){
-      this.msg = notify('hello test world.')
-    })
-    it('shows the notification', function() {
-      assertMessage('normal', 'hello test world.')
-    })
-    after(clearNotification)
-  })
-  describe('normal', function(){
-    describe('long form', function(){
-      before(function(){
-        this.msg = notify('normal', 'hello normal test world.')
-      })
-      it('shows the notification', function() {
-        assertMessage('normal', 'hello normal test world.')
-      })
-      after(clearNotification)
-    })
-    describe('with helper', function(){
-      before(function(){
-        this.msg = notify.normal('hello normal 2 test world.')
-      })
-      it('shows the notification', function() {
-        assertMessage('normal', 'hello normal 2 test world.')
-      })
-      after(clearNotification)
-    })
-  })
+}
 
-  describe('success', function(){
-    describe('long form', function(){
-      before(function(){
-        this.msg = notify('success', 'hello success test world.')
-      })
-      it('shows the notification', function() {
-        assertMessage('success', 'hello success test world.')
-      })
-      after(clearNotification)
-    })
-    describe('with helper', function(){
-      before(function(){
-        this.msg = notify.success('hello success 2 test world.')
-      })
-      it('shows the notification', function() {
-        assertMessage('success', 'hello success 2 test world.')
-      })
-      after(clearNotification)
-    })
-  })
+describe('Notifier', function(){
+  testType('default')
+  testType('normal')
+  testType('error')
+  testType('action')
+  testType('success')
 
-  describe('action', function(){
-    describe('long form', function(){
-      before(function(){
-        this.msg = notify('action', 'hello action test world.')
-      })
-      it('shows the notification', function() {
-        assertMessage('action', 'hello action test world.')
-      })
-      after(clearNotification)
-    })
-    describe('with helper', function(){
-      before(function(){
-        this.msg = notify.action('hello action 2 test world.')
-      })
-      it('shows the notification', function() {
-        assertMessage('action', 'hello action 2 test world.')
-      })
-      after(clearNotification)
-    })
-  })
+  describe('queue', function() {
+    it('queues messages', function() {
+      notify('normal', '1')
+      notify('normal', '2')
+      notify('progress', '3')
+      notify('success', '4')
 
-  describe('with options object', function(){
-    before(function(){
-      this.msg = notify.action({message: 'hello action with options', dismissAfter: 2})
+      assert.equal(notify.queue().length, 4)
     })
-    it('shows the notification', function() {
-      assertMessage('action', 'hello action with options')
-    })
-    it('has the right options set', function(){
-      assert.equal(this.msg.dismissAfter, 2)
-    })
-    after(clearNotification)
-  })
 
-  describe('clear', function(){
-    before(function(done){
-      this.msg = notify.action("test action")
-      notify.clear()
-      this.msg.once('remove', done)
+    it('clears all queued messages', function(done) {
+      notify.clear(true)
+      setTimeout(function() {
+        assert.equal(notify.queue().length, 0)
+        done()
+      }, 20)
     })
-    it('removes notifications', function(){
-      assert.isNull(document.querySelector('.notification'))
+
+    it('removes messages when their time elapses', function(done) {
+      var msg = notify('normal', { message: 'countdown test', safeFor: .02, dismissAfter: .03 })
+      var remaining = msg.autoDismissIn()
+      
+      // Ensure that message is not removed before safeFor elapses
+      setTimeout(function() {
+        assert.equal(notify.queue().length, 1)
+      }, remaining - 1)
+      
+      // Ensure that message is removed after safeFor time elapses
+      setTimeout(function() {
+        assert.equal(notify.queue().length, 0)
+        done()
+      }, remaining + 20)
     })
   })
 })
 
-function clearNotification(done){
-  this.msg.dismiss()
-  setTimeout(done, 300)
-}
